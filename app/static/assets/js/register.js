e0dffcd1391883dbc91034b510b34e6b5312e96b
Ronald <tom20010316@gmail.com>
2023-05-21 17:12:52 +0800

    new change

--- app/static/assets/js/register.js
+++ app/static/assets/js/register.js
@@ -6,22 +6,10 @@ document.getElementById('login__form').addEventListener('submit', function(event
       let confirmPassword =document.getElementById('confirm-pass').value;
     if (password !== confirmPassword) {
       event.preventDefault();
-      alert('两次输入的密码不一致');
+      alert('not same password');
     } else {
       event.preventDefault();
-
-      // var email = document.querySelector('.login__box input[type="email"]').value;
-      // var gender = document.querySelector('.login__check input[name="gender"]:checked').value;
-      // var password = document.querySelector('')
-
-      
-      
-      console.log(username)
-      console.log(password)
-
-
-
-      // Send the form data to the backend
+// Send the form data to the backend
       fetch('http://127.0.0.1:5000/register', {
         method: 'POST',
         headers: {
@@ -30,22 +18,17 @@ document.getElementById('login__form').addEventListener('submit', function(event
         body: JSON.stringify({
           username: username,
           password: password,
-          // gender: gender
         })
-      }).then(response => response.json())
-        .then(data => {
-          console.log('Success:', data);
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
+      }).then(response => {
+        response.json();
+        window.location.href = '/index';  // Add this line
+      }).then(data => {
+        console.log('Success:', data);
+      }).catch((error) => {
+        console.error('Error:', error);
+        window.location.href = '/index';  // Add this line
+      });
     }
-
-
-
-
-
-
 
 
 
